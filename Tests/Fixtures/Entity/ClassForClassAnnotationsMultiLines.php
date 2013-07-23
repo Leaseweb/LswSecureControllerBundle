eb14c87f57273ebd8d64e3d73d22775ed09b93b3
gregoire <gregoirehebert@reflexece.com>
2013-07-23 15:07:51 +0200

    Add Tests

---
+++ Tests/Fixtures/Entity/ClassForClassAnnotationsMultiLines.php
@@ -0,0 +1,18 @@
+<?php
+namespace Lsw\SecureControllerBundle\Tests\Fixtures\Entity;
+
+use Lsw\SecureControllerBundle\Annotation\Secure;
+
+/**
+ * Class ClassForClassAnnotationsMultiLines
+ * Note that any annotation needed for tests is coming from a mock. So these are only visualise what's going on behind.
+ * @Secure(roles="ROLE_USER_EDIT")
+ * @Secure(roles="ROLE_USER_ADD")
+ * @package Lsw\SecureControllerBundle\Tests\Fixtures\Entity
+ * @author Grégoire Hébert <gregoirehebert@reflexece.com>
+ */
+class ClassForClassAnnotationsMultiLines
+{
+    public function stubMethod(){
+    }
+}
