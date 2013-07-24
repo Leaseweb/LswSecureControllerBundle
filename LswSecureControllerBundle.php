f92ee0c13ee467869d08a273f026775acd248ac9
gregoire <gregoirehebert@reflexece.com>
2013-07-24 09:32:16 +0200

    Remove unused use statement

--- LswSecureControllerBundle.php
+++ LswSecureControllerBundle.php
@@ -3,8 +3,6 @@
 namespace Lsw\SecureControllerBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Lsw\SecureControllerBundle\DependencyInjection\LswSecureControllerExtension;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 /**
 * Bundle that provides '@Secure' annotation to secure actions in controllers by specifying required roles.
